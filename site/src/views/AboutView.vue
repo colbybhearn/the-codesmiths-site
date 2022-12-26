06d2d270e5cbdc58516dae3542a3d43891ca65da
colbybhearn <colbybhearn@gmail.com>
2022-12-26 02:15:54 -0600

    outlined content

--- site/src/views/AboutView.vue
+++ site/src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <ul>
+      <li>People over Projects</li>
+      <li>Character over Competence</li>
+      <li>Do unto others...</li>
+      <li>No excuses.</li>
+      <li></li>
+    </ul>
+    
+    
+
   </div>
 </template>
--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    You need {{need}} (character traits)
+
+
+    <br />
+    Service matters.
+
+    <ul>      
+      <li>Tranparency - know status and progress while enjoying less human management.</li>
+      <li>Communication - expect Honesty, Clarity, Responsiveness, </li>
+      <li>Relationships - we are people people, not just computer people.</li>
+    </ul>
+    
+  Skills
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
+  computed:{
+    
+  },
+  data: function () {
+    return {
+      need: "", 
+      needs: [
+        "thinkers",
+        "doers",
+        "self-starters",
+        "coders",
+        "transparency",
+        "communication",
+        "relationships",
+        "integrators",
+        "smart-workers",
+      ]
+    }
+  },
+  mounted: function(){
+    this.changeNeed();
+  },
+  methods:{
+    changeNeed: function(){
+      setTimeout(() => {
+        this.needTimeout();
+        this.changeNeed();
+      }, 1000)
+
+    },
+    needTimeout: function(){
+      let i = Math.random()*this.needs.length;      
+      i = Math.round(i);
+      console.log(i);
+      this.need = this.needs[i];
+      
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
